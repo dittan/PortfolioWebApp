410ac5627902cec5351f054916682393de4cd811
Jacob Andersson <jacobandersson__@hotmail.com>
2018-10-06 12:47:05 +0200

    First commit

---
+++ PortfolioWebApp/wwwroot/assets/js/app.js
@@ -0,0 +1,19 @@
+﻿$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
+
+(function () {
+    $(document).ready(function () {
+        return $(window).scroll(function () {
+            return $(window).scrollTop() > 600 ? $("#back-to-top").addClass("show") : $("#back-to-top").removeClass("show")
+        }), $("#back-to-top").click(function () {
+            return $("html,body").animate({
+                scrollTop: "0"
+            })
+        })
+    })
+}).call(this);
